afb1c6ea49a6cac7ad2afb43245f78c5d77cf39a
zzqi <zzqi@novonordisk.com>
2025-07-09 19:44:19 +0200

    created password and hash columns plus migration

--- API/Program.cs
+++ API/Program.cs
@@ -14,7 +14,7 @@ builder.Services.AddCors();
 
 var app = builder.Build();
 
-app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200","http://localhost:4200"));
+app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200","https://localhost:4200"));
 
 app.MapControllers();
 
