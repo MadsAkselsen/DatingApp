24772d1074260edbfef8c94b0951e3c6c631ca49
zzqi <zzqi@novonordisk.com>
2025-07-09 07:39:14 +0200

    fix fetching data

--- client/src/app/app.ts
+++ client/src/app/app.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
-import { Component, inject } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { lastValueFrom } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -10,12 +11,18 @@ import { Component, inject } from '@angular/core';
 export class App {
   private http = inject(HttpClient);
   protected readonly title = 'client';
+  protected members = signal<any>([]);
 
   ngOnInit() {
-    this.http.get<any[]>('https://jsonplaceholder.typicode.com/posts').subscribe({
-      next: response => console.log(response),
-      error: error => console.error(error),
-      complete: () => console.log('complete')
-    });
+    this.members.set(this.getMembers());
+  }
+
+  async getMembers() {
+    try {
+      return lastValueFrom(this.http.get<any[]>('https://localhost:5001/api/members'));
+    } catch (error) {
+      console.error(error);
+      return []; // Return empty array as fallback
+    }
   }
 }
