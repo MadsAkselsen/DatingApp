a7a4011d5a620985c2db59603d7103d17ebc7e8c
zzqi <zzqi@novonordisk.com>
2025-07-10 07:18:51 +0200

    add controller for registering user

--- API/Controllers/MembersController.cs
+++ API/Controllers/MembersController.cs
@@ -3,27 +3,24 @@ using API.Entities;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
-namespace API.Controllers
+namespace API.Controllers;
+
+public class MembersController(AppDbContext context) : BaseApiController
 {
-    [Route("api/[controller]")] // localhost:5001/api/members
-    [ApiController]
-    public class MembersController(AppDbContext context) : ControllerBase
+    [HttpGet]
+    public async Task<ActionResult<IReadOnlyList<AppUser>>> GetMembers()
     {
-        [HttpGet]
-        public async Task<ActionResult<IReadOnlyList<AppUser>>> GetMembers()
-        {
-            var members = await context.Users.ToListAsync();
-            return Ok(members);
-        }
+        var members = await context.Users.ToListAsync();
+        return Ok(members);
+    }
         
-        [HttpGet("{id}")] // localhost:5001/api/members/bob-id
-        public async Task<ActionResult<AppUser>> GetMember(string id)
-        {
-            var member = await context.Users.FindAsync(id);
+    [HttpGet("{id}")] // localhost:5001/api/members/bob-id
+    public async Task<ActionResult<AppUser>> GetMember(string id)
+    {
+        var member = await context.Users.FindAsync(id);
             
-            if (member == null) return NotFound();
+        if (member == null) return NotFound();
             
-            return Ok(member);
-        }
+        return Ok(member);
     }
 }
