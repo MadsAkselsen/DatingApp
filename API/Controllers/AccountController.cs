cacc29de1671b2408232ef1161e959127b2fac21
zzqi <zzqi@novonordisk.com>
2025-07-12 08:52:08 +0200

    create new db and login

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -1,23 +1,27 @@
 using System.Security.Cryptography;
 using System.Text;
 using API.Data;
+using API.DTOs;
 using API.Entities;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers;
 
 public class AccountController(AppDbContext context) : BaseApiController
 {
     [HttpPost("register")] // api/account/register
-    public async Task<ActionResult<AppUser>> Register(string email, string displayName, string password)
+    public async Task<ActionResult<AppUser>> Register(RegisterDto registerDto)
     {
+        if (await EmailExists(registerDto.Email)) return BadRequest("Email already exists");
+        
         using var hmac = new HMACSHA512();
 
         var user = new AppUser()
         {
-            DisplayName = displayName,
-            Email = email,
-            PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password)),
+            DisplayName = registerDto.DisplayName,
+            Email = registerDto.Email,
+            PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDto.Password)),
             PasswordSalt = hmac.Key
         };
         
@@ -25,5 +29,28 @@ public class AccountController(AppDbContext context) : BaseApiController
         await context.SaveChangesAsync();
         
         return Ok(user);
+    }
+
+    [HttpPost("login")]
+    public async Task<ActionResult<AppUser>> Login(LoginDto loginDto)
+    {
+        var user = await context.Users.SingleOrDefaultAsync(x => x.Email == loginDto.Email);
+        
+        if (user == null) return Unauthorized("Invalid email address");
+        
+        using var hmac = new HMACSHA512(user.PasswordSalt);
+        var computedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(loginDto.Password));
+
+        for (var i = 0; i < computedHash.Length; i++)
+        {
+            if (computedHash[i] != user.PasswordHash[i]) return Unauthorized("Invalid password");
+        }
+        
+        return Ok(user);
+    }
+
+    private async Task<bool> EmailExists(string email)
+    {
+        return await context.Users.AnyAsync(x => x.Email.ToLower() == email.ToLower());
     }
 }
