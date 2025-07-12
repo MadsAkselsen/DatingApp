1d7be31fb332851a4d1c744544a25364a459938b
zzqi <zzqi@novonordisk.com>
2025-07-12 16:01:05 +0200

    more progress on account service

---
+++ client/src/types/user.ts
@@ -0,0 +1,17 @@
+export interface User {
+  id: string;
+  displayName: string;
+  token: string;
+  photoUrl?: string;
+}
+
+export interface LoginCreds {
+  email: string;
+  password: string;
+}
+
+export interface RegisterCreds {
+  displayName: string;
+  username: string;
+  password: string;
+}
