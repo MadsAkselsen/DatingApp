48017d8a761d96b810c4c2dfecbb522e5bf7ad86
zzqi <zzqi@novonordisk.com>
2025-07-13 07:56:13 +0200

    make routes clickable

--- client/src/layout/nav/nav.ts
+++ client/src/layout/nav/nav.ts
@@ -1,15 +1,17 @@
 import { Component, inject, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AccountService } from '../../core/services/account-service';
+import { Router, RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-nav',
-  imports: [FormsModule],
+  imports: [FormsModule, RouterLink, RouterLinkActive],
   templateUrl: './nav.html',
   styleUrl: './nav.css',
 })
 export class Nav {
   protected accountService = inject(AccountService);
+  protected router = inject(Router);
   protected creds: any = {};
 
   login() {
@@ -17,6 +19,7 @@ export class Nav {
       next: (response) => {
         console.log(response);
         this.creds = {};
+        this.router.navigateByUrl('/members');
       },
       error: (error) => {
         console.log(error);
@@ -27,5 +30,6 @@ export class Nav {
 
   logout() {
     this.accountService.logout();
+    this.router.navigateByUrl('/');
   }
 }
