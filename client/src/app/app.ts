a831cee3d568a9472cb6162bb04dfe31bfa709ef
zzqi <zzqi@novonordisk.com>
2025-07-13 09:43:27 +0200

    init route and animations

--- client/src/app/app.ts
+++ client/src/app/app.ts
@@ -1,9 +1,5 @@
-import { HttpClient } from '@angular/common/http';
 import { Component, inject, signal } from '@angular/core';
-import { lastValueFrom } from 'rxjs';
 import { Nav } from "../layout/nav/nav";
-import { AccountService } from '../core/services/account-service';
-import { User } from '../types/user';
 import { Router, RouterModule, RouterOutlet } from '@angular/router';
 
 @Component({
@@ -13,30 +9,5 @@ import { Router, RouterModule, RouterOutlet } from '@angular/router';
   styleUrl: './app.css'
 })
 export class App {
-  private accountService = inject(AccountService);
-  private http = inject(HttpClient);
   protected router = inject(Router);
-  protected readonly title = 'Dating app';
-  protected members = signal<User[]>([]);
-
-  async ngOnInit() {
-    const membersData = await this.getMembers();
-    this.members.set(membersData);
-    this.setCurrentUser();
-  }
-  setCurrentUser() {
-    const user = localStorage.getItem('user');
-    if (user) {
-      this.accountService.currentUser.set(JSON.parse(user));
-    }
-  }
-
-  async getMembers() {
-    try {
-      return lastValueFrom(this.http.get<User[]>('https://localhost:5001/api/members'));
-    } catch (error) {
-      console.error(error);
-      return []; // Return empty array as fallback
-    }
-  }
 }
